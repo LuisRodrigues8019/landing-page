febff6bc27b526aafa70d5468eff42a406495cf2
Rodrigues da Silva <rodriguesdasilva@MacBook-Pro-de-Rodrigues.local>
2022-10-05 16:39:36 +0200

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        "menu": "100px 1fr 100px"
+      }
+    },
+  },
+  plugins: [],
+}
